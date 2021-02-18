ff3dcc9b17fbf2b2e98187d6bd30091302d457d0
liu-yucheng <liu.r.yucheng@gmail.com>
2021-02-19 00:47:36 +0800

    Added part 1

---
+++ p1-open-a-window/main.cpp
@@ -0,0 +1,33 @@
+/* File name: main.cpp
+ *
+ * Reference:
+ * http://ogldev.org/www/tutorial01/tutorial01.html
+ * 
+ * Intro:
+ * A C++ program that initializes the OpenGL context and displays a blank
+ * window on the screen.
+ */
+
+#include <iostream>
+#include <GL/gl.h>
+#include <GL/glut.h>
+
+void display();
+
+int main(int argc, char **argv) {
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
+    glutInitWindowSize(1024, 768);
+    glutInitWindowPosition(100, 100);
+    glutCreateWindow("Open a Window");
+    glutDisplayFunc(display);
+    glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+    glutMainLoop();
+
+    return 0;
+}
+
+void display() {
+    glClear(GL_COLOR_BUFFER_BIT);
+    glutSwapBuffers();
+}
