24a455b11df8c5811c04246b86ea0852d9f51221
liu-yucheng <liu.r.yucheng@gmail.com>
2021-03-10 00:07:43 +0800

    Updated part 5 comments

--- p5-uniform-variables/main.cpp
+++ p5-uniform-variables/main.cpp
@@ -25,9 +25,7 @@
 
 static char const windowTitle[] = "Uniform Variables";
 static GLuint vertexBuffer;
-/* 
- * Reference of the uniform variable "scale" in the vertex shader text.
- */
+/* Reference of the uniform variable "scale" in the vertex shader text. */
 static GLuint scale;
 static char const vertexShaderText[] = R"(
 #version 140
@@ -78,29 +76,23 @@ int main(int argc, char **argv) {
     loadGLUTFuncs();
     // Initialize GLEW
     initGLEW();
-    // Set up the background of the window
+    // Set up the background
     glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
     // Prepare the objects to be drawn
     loadVertexBuffer();
     // Prepare the shaders
     loadShaderProgram();
-    // Draw the objects onto the window
+    // Draw the objects
     glutMainLoop();
 
     return 0;
 }
 
-/*
- * Displays contents in the window.
- */
+/* Displays the objects to be drawn. */
 static void display() {
-    /*
-     * Speed of the rendered object's scale change.
-     */
+    /* Speed of the rendered object's scale change. */
     static float const scaleChangeSpeed = 0.001f;
-    /*
-     * Count of the rendered object's scale changes.
-     */
+    /* Count of the rendered object's scale changes. */
     static int scaleChangeCount = 0;
 
     scaleChangeCount += 1;
@@ -114,33 +106,20 @@ static void display() {
     // Bind the buffer that has the contents to be drawn
     glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
     // Specify the data structure of a vertex to OpenGL
-    // attribute index:                 0 (default)
-    // attribute element count:         3
-    // attribute data type:             float
-    // make attribute normalized:       false
-    // size of vertex data structure:   0 (default)
-    // attribute position in vertex:    0
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
     // Draw the verteices
-    // draw as:             triangles
-    // draw from index:     0
-    // count to be drawn:   3
     glDrawArrays(GL_TRIANGLES, 0, 3);
     glDisableVertexAttribArray(0);
     glutSwapBuffers();
 }
 
-/*
- * Registers the GLUT function callbacks.
- */
+/* Registers the GLUT function callbacks. */
 static void loadGLUTFuncs() {
     glutDisplayFunc(display);
     glutIdleFunc(display);
 }
 
-/*
- * Initializes GLEW.
- */
+/* Initializes GLEW. */
 static void initGLEW() {
     char const funcName[] = "initGLEW";
 
@@ -152,9 +131,7 @@ static void initGLEW() {
     }
 }
 
-/*
- * Initializes the vertex buffer.
- */
+/* Initializes the vertex buffer. */
 static void loadVertexBuffer() {
     // Create array of vertices
     int const verticesCount = 3;
@@ -273,9 +250,7 @@ static void addShaderTextToProgram(
     glAttachShader(shaderProgram, shader);
 }
 
-/*
- * Show an error information line in stderr.
- */
+/* Shows an error information line in stderr. */
 static void errShowLine(char const *funcName, char const *format, ...) {
     va_list args;
     va_start(args, format);
@@ -290,9 +265,7 @@ static void errShowLine(char const *funcName, char const *format, ...) {
     va_end(args);
 }
 
-/*
- * Get and show the GL shader program info log in stderr.
- */
+/* Gets and shows the GL shader program info log in stderr. */
 static void errShowProgramLog(char const *funcName, GLuint program) {
     int const logLength = 1023;
     GLchar log[logLength + 1] = {0};
@@ -300,9 +273,7 @@ static void errShowProgramLog(char const *funcName, GLuint program) {
     errShowLine(funcName, "info log: %s", log);
 }
 
-/*
- * Get and show the GL shader info log in stderr.
- */
+/* Gets and shows the GL shader info log in stderr. */
 static void errShowShaderLog(char const *funcName, GLuint shader) {
     int const logLength = 1023;
     GLchar log[logLength + 1] = {0};
